a1f7897df9f1b792cd66721d743d75ed37711c40
月白 <2249351762@qq.com>
2023-02-25 21:36:01 +0800

    1

--- main.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById('btn_click');
-var title = document.getElementById("title");
-
-var count = 0;
-
-btn.onclick = ev => {
-    count++;
-    title.textContent = count;
-};
